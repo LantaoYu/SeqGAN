fdc798c124299f2793fb7dbcd2b01a834bad0bb2
Jozef Mokry <jozef.mokry@gmail.com>
2017-02-09 12:19:11 +0000

    Ui should be a matrix from hidden state dim to hidden state dim

--- MLE_SeqGAN/model.py
+++ MLE_SeqGAN/model.py
@@ -142,7 +142,7 @@ class LSTM(object):
     def create_recurrent_unit(self, params):
         # Weights and Bias for input and hidden tensor
         self.Wi = tf.Variable(self.init_matrix([self.emb_dim, self.hidden_dim]))
-        self.Ui = tf.Variable(self.init_matrix([self.emb_dim, self.hidden_dim]))
+        self.Ui = tf.Variable(self.init_matrix([self.hidden_dim, self.hidden_dim]))
         self.bi = tf.Variable(self.init_matrix([self.hidden_dim]))
 
         self.Wf = tf.Variable(self.init_matrix([self.emb_dim, self.hidden_dim]))
